ac0fd8844e4488af4549facdf0d4a12713c1cfce
Rohan Sharma <69635604+rohansharma4050@users.noreply.github.com>
2026-04-27 17:16:32 -0400

    removing the mongo key

--- drug-mapping-game/src/lib/mongodb.js
+++ drug-mapping-game/src/lib/mongodb.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb'
 
-const uri = process.env.MONGODB_URI || "mongodb+srv://***REDACTED***@oncref.se0af.mongodb.net/?retryWrites=true&w=majority&appName=OncRef"
+const uri = process.env.MONGODB_URI
 
 let client
 let clientPromise
